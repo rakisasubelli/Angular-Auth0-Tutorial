40691865e5655972f05038542ffbe65a90ec55a4
ramakrishna s <ramakrishna.s@jarustech.com>
2017-06-18 20:09:22 +0530

    angular js components
    components for angular js authandication app

---
+++ components/profile/profile.ctr.js
@@ -0,0 +1,10 @@
+(function (){
+	'use strict';
+	angular.module('authApp')
+	.controller('profileController', profileController);
+
+	function profileController($http) {
+		var vm = this;
+		vm.message = "Hello";
+	}
+})();
